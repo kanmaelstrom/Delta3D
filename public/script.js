7ebfd508baea89936616d5b4aef5133282b66f9a
wowdog15 <kanmaelstrom@gmail.com>
2025-11-21 16:15:45 -0500

    push

--- public/script.js
+++ public/script.js
@@ -1,5 +1,15 @@
 fetch("/resources/items.json")
   .then(response => response.json())
   .then(items => {
-    console.log(items);
-  });
+    items.forEach(item => {
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <h3>${item.name}</h3>
+            <p>Price: $${item.price}</p>
+            <p>${item.description}</p>
+            <img src="${item.image}" alt="${item.name}" width="150">
+        `;
+        document.body.appendChild(div);
+    });
+  })
+  .catch(err => console.error("Failed to load items:", err));
